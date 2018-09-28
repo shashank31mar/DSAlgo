9f9e85c7ec15e5172150f04d7b3d6abb036585ae
Shashank GUPTA <shashank.gupta@amadeus.com>
2018-09-28 16:04:14 +0530

    Code updated

--- CostSplittingApp.py
+++ CostSplittingApp.py
@@ -1,24 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 27 20:01:51 2018
+Created on Thu Sep 28 13:01:51 2018
 
 @author: shashankgupta
 """
 
-from heap import MinHeap, MaxHeap
+import GenericHeap
 
 class CostSplittingApp:
     def __init__(self):
         pass
     
     def settleDebt(self,creditors,debitors):
-        pass
+        c_maxH = GenericHeap.MaxGHeap(creditors)
+        c_maxH.convertToHeap()
+        d_maxH = GenericHeap.MaxGHeap(debitors)
+        d_maxH.convertToHeap()
+        #c_maxH.printHeap()
+        #d_maxH.printHeap()
+        self.settleDebtUtil(c_maxH,d_maxH)
     
-    def settleDebtUtil(self,minH,maxH):
-        pass
-    
+    def settleDebtUtil(self,c_maxH,d_maxH):
+        while c_maxH.heap_size > 0:
+            min_val = min(c_maxH.getMax().key,d_maxH.getMax().key)
+            c_max_name, c_max_val = c_maxH.getMax().name, c_maxH.getMax().key
+            d_max_name, d_max_val = d_maxH.getMax().name, d_maxH.getMax().key
+            
+            #print("c_max_val :{}, d_max_val :{}, min_val :{}".format(c_max_val, d_max_val, min_val))
 
+            if c_max_name == d_max_name:
+                c_maxH.extractMax()
+                d_maxH.extractMax()
+                continue
+
+            if c_max_val == 0:
+                c_maxH.extractMax()
+                continue
+            
+            if (c_max_val - min_val) == 0:
+                c_maxH.extractMax()
+            else:
+                c_maxH.updateMaxKey(c_maxH.getMax().key - min_val)
+            
+            if (d_max_val - min_val) == 0:
+                d_maxH.extractMax()
+            else:
+                d_maxH.updateMaxKey(d_maxH.getMax().key - min_val)
+                
+            #c_maxH.printHeap()
+            #d_maxH.printHeap()
+            print("{} --> {} --> {}".format(d_max_name, c_max_name, min_val))
+            
 def main():
     '''
     input syntax:
@@ -28,7 +61,17 @@ def main():
         B 700 0
         C 0   500
     '''
-    pass
+    creditors = {'A':100, 'B':200, 'C':200, 'D':100}
+    debitors = {'A':200, 'B':100, 'C':200, 'D':100}
+    
+    creditors1 = {'A':500, 'B':0, 'C':500}
+    debitors1 = {'A':300, 'B':700, 'C':0}
+    
+    creditors2 = {'A':1000, 'B':1000, 'C':0, 'D':0, 'E':0}
+    debitors2 = {'A':0, 'B':300, 'C':700, 'D':500, 'E':500}
+    
+    csa = CostSplittingApp()
+    csa.settleDebt(creditors2, debitors2)
 
 if __name__ == "__main__":
     main()
--- GenericHeap.py
+++ GenericHeap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 27 21:03:13 2018
+Created on Thu Sep 28 12:15:13 2018
 
 @author: shashankgupta
 """
@@ -19,17 +19,14 @@ class Node:
         return self.name + "-" + str(self.key)
         
 class GenericHeap:
-    def __init__(self,capacity=10,arr=[]):
+    def __init__(self,arr={},capacity=10):
         print("Initializing : " + type(self).__name__)
         self.heap = []
         self.capacity = capacity
         self.heap_size = 0
-        if not arr:
-            self.heap = [None]*capacity
-        else:
-            for x in arr:
-                self.heap.append(x)
-            self.capacity = capacity
+        self.heap = [None]*capacity
+        self.arr = arr
+        #self.convertToHeap(arr)
     
     def parent(self,i):
         return (i-1)//2
@@ -50,7 +47,13 @@ class GenericHeap:
     def insert(self,key):
         #print("size :{}, cap :{}".format(self.heap_size,self.capacity))
         if self.heap_size == self.capacity:
-            print("Heap is full, not inserting!!")
+            print("Heap is full!")
+            inp = input("Do you want to increase the capacity of heap?(Y/N):").lower()
+            if inp == 'y' or inp == 'yes':
+                self.capacity += 10
+            elif inp == 'n' or inp == 'no':
+                print("not inserting curr element!!")
+            
             return None
         self.heap_size += 1
         i = self.heap_size - 1
@@ -63,9 +66,9 @@ class GenericHeap:
             self.heap[i].printData()
         #print(self.heap[:self.heap_size])
         
-    def convertToHeap(self,dic):
+    def convertToHeap(self):
         print(type(self).__name__)
-        for k,v in dic.items():
+        for k,v in self.arr.items():
             n = Node(v,k)
             self.insert(n)
 
@@ -73,7 +76,7 @@ class MinGHeap(GenericHeap):
     def getMin(self):
         if self.heap_size > 0:
             print(type(self).__name__[:3] + " is :", end=" ")
-            return self.heap[0].getData()
+            return self.heap[0]
         else:
             print("Heap is empty!")
             return None
@@ -117,12 +120,84 @@ class MinGHeap(GenericHeap):
     def replaceMin(self,key):
         self.heap[0] = key
         self.minHeapify(0)
+        
+    def isEmpty(self):
+        if self.heap_size == 0:
+            return True
+        return False
 
+class MaxGHeap(GenericHeap):
+    def getMax(self):
+        if self.heap_size > 0 :
+            #print(type(self).__name__[:3] + " is :", end=" ")
+            return self.heap[0]
+        else:
+            print("Heap is empty!")
+            return None
+        
+    def extractMax(self):
+        if self.heap_size <= 0:
+            print("Heap is empty!!")
+            return None
+        
+        if self.heap_size == 1:
+            self.heap_size -= 1
+            return self.heap[0].getData()
+        
+        max_ele = self.heap[0].getData()
+        self.heap[0] = self.heap[self.heap_size - 1]
+        self.heap_size -= 1
+        self.maxHeapify(0)
+        return max_ele
+    
+    def maxHeapify(self,index=0):
+        l = self.left(index)
+        r = self.right(index)
+        largest = index
+        
+        if l < self.heap_size and self.heap[l].key > self.heap[index].key:
+            largest = l
+        if r < self.heap_size and self.heap[r].key > self.heap[largest].key:
+            largest = r
+            
+        if largest != index:
+            self.heap[largest], self.heap[index] = self.heap[index], self.heap[largest]
+            self.maxHeapify(largest)
+            
+    def fixHeap(self,index):
+        while index !=0 and self.heap[self.parent(index)].key < self.heap[index].key:
+            self.heap[self.parent(index)], self.heap[index] = self.heap[index],\
+            self.heap[self.parent(index)]
+            index = self.parent(index)
+
+    def replaceMax(self,key):
+        self.heap[0] = key
+        self.maxHeapify(0)
+        
+    def updateMaxKey(self,val):
+        self.heap[0].key = val
+        self.maxHeapify(0)
+        
+    def isEmpty(self):
+        if self.heap_size == 0:
+            return True
+        return False
+    
+    def delete(self,index):
+        self.decreaseKey(index,math.inf)
+        self.extractMin()
+        
 def main():
     dic = {'A':40,'B':30,'C':20,'D':10}
-    minH = MinGHeap()
-    minH.convertToHeap(dic)
+    minH = MinGHeap(dic)
+    minH.convertToHeap()
     minH.printHeap()
+    maxH = MaxGHeap(dic)
+    maxH.convertToHeap()
+    maxH.printHeap()
+    maxH.updateMaxKey(-10)
+    maxH.printHeap()
+    
     names = ['X','Y','Z']
     for i in range(3):
         print("Min ele is : {}".format(minH.extractMin()))
@@ -134,5 +209,8 @@ def main():
     print("Min ele is : {}".format(minH.extractMin()))
     print("Min ele is : {}".format(minH.extractMin()))
     print("Min ele is : {}".format(minH.extractMin()))
+    print("is empty : {}".format(maxH.isEmpty()))
+    minH.printHeap()
+    
 if __name__ == "__main__":
     main()
